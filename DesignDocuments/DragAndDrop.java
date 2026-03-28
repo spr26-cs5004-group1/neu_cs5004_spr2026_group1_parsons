d558ca9ca1b2f4e329f919e0564208bd77faed48
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-03-28 11:00:49 -0700

    two steps in drag and drop

--- DesignDocuments/DragAndDrop.java
+++ DesignDocuments/DragAndDrop.java
@@ -10,17 +10,35 @@ public class DragAndDrop {
     private static final int DIVIDER_LOCATION = 1280 / 2;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Parsons Problems");
-        frame.setLayout(new GridLayout(1, 2));
-        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setDividerLocation(DIVIDER_LOCATION);
-        JPanel blocksPanel = new JPanel(new GridLayout(20, 1, 5, 5));  // allowing 20 code blocks
-        JPanel answerPanel = new JPanel(new GridLayout(20, 1, 5, 5));
-        JScrollPane leftScroll = new JScrollPane(blocksPanel);
-        JScrollPane rightScroll = new JScrollPane(answerPanel);
-        frame.add(leftScroll);
-        frame.add(rightScroll);
-        frame.setVisible(); 
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Parsons Problems");
+            frame.setLayout(new BorderLayout());
+            frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JTextArea instrLabel = new JtextArea("Instructions\n" 
+                + "Related to: While loop in Java.\n"
+                + "Add Code Blocks from left to the right panel"
+                + "to make the correct sequence.");
+            JPanel instrPanel = new JPanel(new FlowLayout());
+            instrPanel.add(instrLabel);
+
+            // two panels side by side
+            // allowing 20 code blocks in height
+            JPanel blocksPanel = new JPanel(new GridLayout(20, 1, 5, 5));  
+            JPanel answerPanel = new JPanel(new GridLayout(20, 1, 5, 5));
+            JScrollPane leftScroll = new JScrollPane(blocksPanel);
+            JScrollPane rightScroll = new JScrollPane(answerPanel);
+            JSplitPane splitPane = new JSplitPane(
+            JSplitPane.HORIZONTAL_SPLIT,
+                leftScroll,
+                rightScroll
+            );
+            splitPane.setDividerLocation(DIVIDER_LOCATION);
+            // add to frame in sequence
+            frame.add(instrPanel, BorderLayout.NORTH);
+            frame.add(splitPane, BorderLayout.CENTER);
+            frame.setVisible(true);
+        });
     }
 }
