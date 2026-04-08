7ae56809227a22f0e137901996c6cbf622917745
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-08 09:41:01 -0700

    Update SolverView.java

--- src/main/java/com/parsons/controller/student/SolverView.java
+++ src/main/java/com/parsons/controller/student/SolverView.java
@@ -1,1 +1,10 @@
-SolverView.java
+package com.parsons.controller.student;
+import com.parsons.controller.GuiConstants;
+import com.parsons.model.ParsonsProblem;
+import javax.swing.*;
+
+public class SolverView extends JFrame implements GuiConstants{
+    public SolverView(ParsonsProblem problems) {
+
+    }
+}
