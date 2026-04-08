2e08af780a372f41c17ab7f9654110590ed7108a
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-08 11:11:03 -0700

    Update SolverViewTest.java

--- src/test/java/com/parsons/controller/student/SolverViewTest.java
+++ src/test/java/com/parsons/controller/student/SolverViewTest.java
@@ -1,4 +1,11 @@
 package com.parsons.controller.student;
+import com.parsons.controller.DummyData;
+import javax.swing.*;
 
 public class SolverViewTest {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() ->
+                new SolverView(DummyData.getProblems().get(0))
+        );
+    }
 }
