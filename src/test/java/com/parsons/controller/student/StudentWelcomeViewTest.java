014ed35e157f51ddba91b9114b32334c6867a050
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-08 19:00:59 -0700

    reformat calling StudentWelcomeView

--- src/test/java/com/parsons/controller/student/StudentWelcomeViewTest.java
+++ src/test/java/com/parsons/controller/student/StudentWelcomeViewTest.java
@@ -11,7 +11,7 @@ public class StudentWelcomeViewTest {
     private static final ParsonsProblemsService service = new ParsonsProblemsService(repository);
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() ->
-                new StudentWelcomeView(service.getAllProblems(), service)
+                new StudentWelcomeView(service)
         );
     }
 }
